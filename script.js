927d711074f7d5143b53e1f51e794cf08727128a
mkatcoder <maryia.katanovich@gmail.com>
2024-09-26 09:47:49 +0300

    Домашнее задание 10

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
 const form = document.querySelector(".form");
+const submitButton = document.querySelector("#button");
+const clearButton = document.querySelector("#clear");
+
 form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
 
-  // Здесь твой код
+  fetch(`https://polinashneider.space/user`, {
+    method: 'POST', 
+    headers: {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer: mkatcoder' 
+    },
+
+    body: JSON.stringify({
+      "name": document.querySelector("#name").value,
+      "secondName": document.querySelector("#secondName").value,
+      "phone": document.querySelector("#phone").value,
+      "email": document.querySelector("#email").value,
+      "agree": document.querySelector("#agree").checked
+    }),
+
+  })
+  
+  .then((result) => {
+    return result.json();
+})  
+
+  .then((reply) => {
+      return notifyUser("Данные успешно отправлены!", "success");
+      form.reset();
+    })
+
+  .catch((error) => {
+    return notifyUser("Ошибка при отправке данных на сервер", "failure");
+  });
 });
+
+function notifyUser(message, type) {
+  const notification = document.createElement("div");
+  if (type === "success") {
+    notification.classList.add("success"); //добавила в CSS .success
+  } else {
+    notification.classList.add("failure"); // //добавила в CSS .failure
+  }
+  
+  notification.textContent = message;
+
+  form.append(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+}
